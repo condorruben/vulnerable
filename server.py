b57b68644732e90a7f3ccc3462f2fd90fac493d6
CONDOR Ruben Andrei <condor.ruben@gmail.com>
2022-03-11 14:46:23 +0200

    add vulnerable server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    operation = request.form["operation"]
+    result = eval(operation)
+    return render_template("index.html", result=str(result))
+
+if __name__ == "__main__":
+    app.run()
